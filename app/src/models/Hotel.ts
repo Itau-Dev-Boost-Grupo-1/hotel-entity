495ea5d88416b745ec5ae883492f2edc29dea292
Adrian Murillo <adrianmurillo@pecege.com>
2020-09-17 20:51:06 -0300

    add hotel entity

---
+++ app/src/models/Hotel.ts
@@ -0,0 +1,47 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import Room from "./Rooms";
+
+// Entidade representa uma tables no DB
+@Entity("hotels")
+export default class User {
+    // chave primaria auto incremental
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    nome: string;
+
+    @Column()
+    descrição: string;
+    
+    @Column()
+    endereço: string;
+    
+    @Column()
+    cidade: string;
+    
+    @Column()
+    estrelas: number;
+    
+    @Column()
+    photo_url: string;
+    
+    @ManyToOne(type => Room, room => room.id)
+    room: Room;
+
+    constructor(
+        nome: string, 
+        descrição: string, 
+        endereço: string, 
+        cidade: string, 
+        estrelas:number, 
+        photo_url: string)
+        {
+        this.nome = nome;
+        this.descrição = descrição;
+        this.endereço = endereço;
+        this.cidade = cidade;
+        this.estrelas = estrelas;
+        this.photo_url = photo_url;
+        }
+}
